3b7fe5607903dd93d53aae2b6ac38af0bea4764d
mark-soro <napoleon_soro@rocketmail.com>
2020-08-11 14:50:51 -0400

    added colt's solution

--- app.js
+++ app.js
@@ -10,14 +10,38 @@ app.get('/speak/:animal', function(req, res){
   var animalSounds = animal == 'pig' ? 'Oink'
                    : animal == 'cow' ? 'Moo'
                    : animal == 'dog' ? 'Woof Woof!'
-                   : 'leave me alone!';
-  res.send(`The ${animal} says ${animalSounds}`);
+                   : animal == 'cat' ? 'I hate you hooman!'
+                   : animal == 'goldfish' ? '...'
+                   : '';
+  res.send(`The ${animal} says '${animalSounds}'`);
+
+  // OR like Colt's solution
+  // var sounds = {
+  //     pig: 'Oink',
+  //     cow: 'Moo',
+  //     dog: 'Woof Woof!',
+  //     cat: 'I hate you hooman',
+  //     goldfish: '...'
+  // }
+  // var animal = req.params.animal.toLowerCase();
+  // var sound = sounds[animal];
+  // res.send(`The ${animal} says '${sound}'`);
 });
 
 app.get('/repeat/:anyString/:numberOfTimes', function(req, res){
   var anyString = req.params.anyString.concat(" ");
   var numberOfTimes = req.params.numberOfTimes;
   res.send(`${anyString.repeat(numberOfTimes)}`);
+
+  // OR like Colt's solution
+  // var anyString = req.params.anyString;
+  // var numberOfTimes = Number(req.params.numberOfTimes);
+  // var result = '';
+  //
+  // for(var i = 0; i < numberOfTimes; i++){
+  //   result += anyString + ' ';
+  // }
+  // res.send(result);
 });
 
 app.get('*', function(req, res){
